3f39636944fcd22135e6aff9bb209f2ed595524b
Sermet <sermetkose@gmail.com>
2020-12-07 18:57:20 +0300

    remove autofac.

--- OouiSignalRSample/Startup.cs
+++ OouiSignalRSample/Startup.cs
@@ -3,63 +3,35 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OouiSignalRSample.Core.Hubs;
-using OouiSignalRSample.IOC;
 using Xamarin.Forms;
 
 namespace OouiSignalRSample
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(options => options.EnableEndpointRouting = false);
             services.AddSignalR();
-            //services.AddSignalR().AddAzureSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            //app.UseStaticFiles();
-
+            app.UseExceptionHandler("/Error");
             app.UseOoui();
-
-            var appSetup = new AppSetup();
-            AppContainer.Container = appSetup.CreateContainer();
-
+            app.UseRouting();
             Forms.Init();
-
-            app.UseMvc(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            //app.UseAzureSignalR(routes =>
-            //{
-            //    routes.MapHub<ChatHub>("/chat");
-            //});
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<ChatHub>("/chat");
+                endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chat");
             });
         }
     }
